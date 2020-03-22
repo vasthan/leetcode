23776ae8582f7902b21f42b34b19fd9ab05ddfc7
yanghanqing <imyanghanqing@gmail.com>
2020-03-22 14:57:26 +0800

    intersection of two arrays

---
+++ src/main/java/com/adc/_300_399/_349_intersection_of_two_arrays/Solution.java
@@ -0,0 +1,29 @@
+package com.adc._300_399._349_intersection_of_two_arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> result = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                result.add(num);
+            }
+        }
+
+        int[] intersection = new int[result.size()];
+        int index = 0;
+        for (int v : result) {
+            intersection[index++] = v;
+        }
+        return intersection;
+    }
+}
