c18da2e81e7f4368967550657cc5a3009c1c1281
yanghanqing <imyanghanqing@gmail.com>
2020-06-16 13:47:12 +0800

    binary search

---
+++ src/main/java/com/adc/_300_max/_704_binary_search/Solution.java
@@ -0,0 +1,46 @@
+package com.adc._300_max._704_binary_search;
+/*
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+示例 1:
+
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+示例 2:
+
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+ 
+
+提示：
+
+你可以假设 nums 中的所有元素是不重复的。
+n 将在 [1, 10000]之间。
+nums 的每个元素都将在 [-9999, 9999]之间。
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-search
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int l = 0, r = nums.length - 1;
+        if (target < nums[l] || target > nums[r]) return -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
