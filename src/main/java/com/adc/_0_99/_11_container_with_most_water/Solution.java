fde6cfb365c0bd88a7f2c9f2395b7738f070802a
yanghanqing <imyanghanqing@gmail.com>
2020-06-18 18:09:07 +0800

    11 optimize

--- src/main/java/com/adc/_0_99/_11_container_with_most_water/Solution.java
+++ src/main/java/com/adc/_0_99/_11_container_with_most_water/Solution.java
@@ -5,25 +5,24 @@ package com.adc._0_99._11_container_with_most_water;
  */
 class Solution {
     public int maxArea(int[] height) {
+        if (height == null || height.length == 1)
+            return 0;
         int i = 0, j = height.length - 1;
-        int max = (j - i) * Math.min(height[i], height[j]);
+        int maxArea = area(height, i, j);
         while (i < j) {
             int hi = height[i];
             int hj = height[j];
-            if (hi < hj) {
-                while (i < j && height[i] <= hi) {
-                    i++;
-                }
-            } else {
-                while (i < j && height[j] <= hj) {
-                    j--;
-                }
-            }
-            if (i < j) {
-                int area = (j - i) * Math.min(height[i], height[j]);
-                max = Math.max(max, area);
-            }
+            if (hi < hj)
+                while (i < j && height[i] <= hi) i++;
+            else
+                while (i < j && height[j] <= hj) j--;
+            if (i < j)
+                maxArea = Math.max(maxArea, area(height, i, j));
         }
-        return max;
+        return maxArea;
+    }
+
+    private int area(int[] height, int i, int j) {
+        return (j - i) * Math.min(height[i], height[j]);
     }
 }
