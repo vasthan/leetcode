a1d083a483938536fb0517ef0c6a6a8583101fb7
yanghanqing <imyanghanqing@gmail.com>
2020-06-19 18:10:02 +0800

    148 sort list

---
+++ src/main/java/com/adc/_100_199/_148_sort_list/Solution.java
@@ -0,0 +1,110 @@
+package com.adc._100_199._148_sort_list;
+
+
+/*
+148. 排序链表
+在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+
+示例 1:
+
+输入: 4->2->1->3
+输出: 1->2->3->4
+示例 2:
+
+输入: -1->5->3->4->0
+输出: -1->0->3->4->5
+ */
+
+// 使用归并排序
+class Solution {
+    // 自顶向下归并
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        // 寻找中点
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode head2 = slow.next;
+        slow.next = null;
+
+        // 递归排序左右两部分
+        head = sortList(head);
+        head2 = sortList(head2);
+
+        // 合并
+        return merge(head, head2);
+    }
+
+    // 自底向上归并
+    public ListNode sortListBottomUp(ListNode head) {
+
+        return null;
+    }
+
+
+    private ListNode merge(ListNode a, ListNode b) {
+        // 分别指向两个链表头节点
+        ListNode p1 = a, p2 = b;
+        // 虚拟头节点
+        ListNode dummyHead = new ListNode(-1);
+        // 合并操作的指针，新链表的尾节点
+        ListNode p = dummyHead;
+
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                ListNode next = p1.next;
+                p1.next = null;
+                p.next = p1;
+                p1 = next;
+            } else {
+                ListNode next = p2.next;
+                p2.next = null;
+                p.next = p2;
+                p2 = next;
+            }
+            p = p.next;
+        }
+        if (p1 != null) p.next = p1;
+        if (p2 != null) p.next = p2;
+        return dummyHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(4);
+        ListNode b = new ListNode(3);
+        ListNode c = new ListNode(2);
+        ListNode d = new ListNode(1);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+        printListNode(a);
+        ListNode a2 = new Solution().sortList(a);
+        printListNode(a2);
+    }
+
+    private static void printListNode(ListNode head) {
+        ListNode cur = head;
+        StringBuilder sb = new StringBuilder();
+        while (cur != null) {
+            sb.append(cur.val);
+            if (cur.next != null) {
+                sb.append("->");
+            }
+            cur = cur.next;
+        }
+        System.out.println(sb.toString());
+    }
+}
