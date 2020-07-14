d93c7da3f1b7c17063c9f3b3b07f25710d45d41d
yanghanqing <imyanghanqing@gmail.com>
2020-07-14 16:59:25 +0800

    235 lowest common ancestor of bst

---
+++ src/main/java/com/adc/_200_299/_235_lowest_common_ancestor_of_bst/Solution.java
@@ -0,0 +1,29 @@
+package com.adc._200_299._235_lowest_common_ancestor_of_bst;
+
+/*
+二叉搜索树的最近公共祖先
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
