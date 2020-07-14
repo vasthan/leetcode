e3b137ca6043f742ec23c2ac5da0846b4969f515
yanghanqing <imyanghanqing@gmail.com>
2020-07-14 15:06:17 +0800

    300 最长上升子序列

---
+++ src/main/java/com/adc/_300_max/_300_longest_increasing_subsequence/Solution1.java
@@ -0,0 +1,45 @@
+package com.adc._300_max._300_longest_increasing_subsequence;
+
+import java.util.Arrays;
+
+/*
+300. 最长上升子序列
+给定一个无序的整数数组，找到其中最长上升子序列的长度。
+
+示例:
+
+输入: [10,9,2,5,3,7,101,18]
+输出: 4
+解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+说明:
+
+可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+你算法的时间复杂度应该为 O(n2) 。
+进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+ */
+public class Solution1 {
+
+    // 时间复杂度O(n^2)
+    // dp = new int[nums.length]，dp[i]表示以nums[i]结尾的最长上升子序列的长度
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int res = dp[0];
+        for (int i = 1; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
