8040ac1844f6ef05c4c5d8049ec87ac05d1d02ee
yanghanqing <imyanghanqing@gmail.com>
2021-11-07 13:42:20 +0800

    lc146: lru cache

---
+++ src/main/java/com/adc/_100_199/_146_lru_cache/LRUCache.java
@@ -0,0 +1,18 @@
+package com.adc._100_199._146_lru_cache;
+
+/**
+ * 原题：https://leetcode-cn.com/problems/lru-cache/
+ */
+public class LRUCache {
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
